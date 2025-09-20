9a08b8678b7cb17e0915868b163fa8fe4a59d335
myuyen0304 <134291922+myuyen0304@users.noreply.github.com>
2025-09-20 21:49:50 +0700

    Init basic server nodejs

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(express.json());
+
+// Kết nối MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/restful-session-app")
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error(err));
+
+// Session middleware
+app.use(
+  session({
+    secret: "mySecretKey",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/restful-session-app" }),
+    cookie: { maxAge: 1000 * 60 * 60 } // 1 giờ
+  })
+);
+
+// Routes
+app.use("/auth", authRoutes);
+
+app.listen(3000, () => {
+  console.log("🚀 Server running at http://localhost:3000");
+});
