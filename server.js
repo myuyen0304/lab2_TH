cba59759be1197024f4224ae54646af1744bfd7f
myuyen0304 <134291922+myuyen0304@users.noreply.github.com>
2025-09-20 22:58:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,25 +2,34 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
+const cookieParser = require("cookie-parser");
 
 const authRoutes = require("./routes/auth");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 // Kết nối MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/sessionAuth")
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error(err));
 
-// Session middleware
+// Session middleware (cookie-based)
 app.use(
   session({
+    name: "sessionId", // tên cookie
     secret: "mySecretKey",
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/sessionAuth" }),
-    cookie: { maxAge: 1000 * 60 * 60 } // 1 giờ
+    store: MongoStore.create({
+      mongoUrl: "mongodb://127.0.0.1:27017/restful-session-app",
+    }),
+    cookie: {
+      httpOnly: true,   // không cho JS đọc cookie
+      secure: false,    // true nếu chạy HTTPS
+      maxAge: 1000 * 60 * 60, // 1h
+    },
   })
 );
 
